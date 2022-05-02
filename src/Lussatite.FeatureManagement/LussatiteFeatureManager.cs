8eaeb442d298074685f9fa6123894fef31b3f9cd
Thomas Harold <tgharold@gmail.com>
2022-05-02 06:50:15 -0400

    Add IReadOnlyFeatureValueProvider concept

--- src/Lussatite.FeatureManagement/LussatiteFeatureManager.cs
+++ src/Lussatite.FeatureManagement/LussatiteFeatureManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -8,14 +9,17 @@ namespace Lussatite.FeatureManagement
 {
     public class LussatiteFeatureManager : IFeatureManager
     {
+        private readonly IEnumerable<IReadOnlyFeatureValueProvider> _readOnlyFeatureValueProviders;
         private readonly List<string> _featureNames;
-        private readonly string _sectionName = "FeatureManagement";
 
         public LussatiteFeatureManager(
-            IEnumerable<string> featureNames
+            IEnumerable<string> featureNames,
+            IEnumerable<IReadOnlyFeatureValueProvider> readOnlyFeatureValueProviders
             )
         {
             _featureNames = featureNames?.ToList() ?? new List<string>();
+            _readOnlyFeatureValueProviders = readOnlyFeatureValueProviders 
+                ?? throw new ArgumentNullException(nameof(readOnlyFeatureValueProviders));
         }
         
         /// <inheritdoc cref="IFeatureManager.GetFeatureNamesAsync"/>
@@ -28,12 +32,13 @@ namespace Lussatite.FeatureManagement
         /// <param name="feature">The name of the feature to check.  If the name was not
         /// registered in the constructor, it will always return false.</param>
         /// <returns>True if the feature is enabled, otherwise false.</returns>
-        public virtual Task<bool> IsEnabledAsync(string feature)
+        public virtual async Task<bool> IsEnabledAsync(string feature)
         {
-            return Task.FromResult(GetFeatureValueFromConfigurationManager(feature));
+            return await GetFeatureValueFromProviders(feature);
         }
 
-        /// <summary>Checks whether a given feature is enabled.</summary>
+        /// <summary>WARNING: This is not yet implemented.
+        /// Checks whether a given feature is enabled.</summary>
         /// <param name="feature">The name of the feature to check.  If the name was not
         /// registered in the constructor, it will always return false.</param>
         /// <param name="context">A context providing information that can be used to evaluate whether a feature should be on or off.</param>
@@ -49,19 +54,17 @@ namespace Lussatite.FeatureManagement
                 && _featureNames?.Any(x => x == feature) == true;
         }
 
-        protected bool GetFeatureValueFromConfigurationManager(string feature)
+        private async Task<bool> GetFeatureValueFromProviders(string feature)
         {
             if (!FeatureIsRegistered(feature)) return false;
-            var stringValue = ConfigurationManager.AppSettings[FeaturePath(feature)];
             
-            //TODO: Need to write the code to handle both web.config XML style "sectionName:featureName" and app.config / appsettings.config JSON files
-            
-            return bool.TryParse(stringValue, out var boolResult) && boolResult;
-        }
+            foreach (var valueProvider in _readOnlyFeatureValueProviders)
+            {
+                var result = await valueProvider.GetAsync(feature).ConfigureAwait(false);
+                if (result.HasValue) return result.Value;
+            }
 
-        private string FeaturePath(string feature)
-        {
-            return $"{_sectionName}.{feature}";
+            return false;
         }
     }
 }
