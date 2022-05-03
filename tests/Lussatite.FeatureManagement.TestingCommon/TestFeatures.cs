a3585ec22579d5eb3bef6d6d5d1cb63b4df1070c
Thomas Harold <tgharold@gmail.com>
2022-05-03 13:38:36 -0400

    Update the common test constant files

--- tests/Lussatite.FeatureManagement.TestingCommon/TestFeatures.cs
+++ tests/Lussatite.FeatureManagement.TestingCommon/TestFeatures.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
@@ -10,12 +9,6 @@ public static class TestFeatures
 {
     // avoid feature flag names with spaces, Pascal-case is good, sticking to just letters/numbers is good.
     
-    [Description("A: some longer explanation goes here")]
-    public const string A = nameof(A);
-
-    [Description("Fancy New Interface: New and improved! Now with Brawndo!")]
-    public const string FancyNewInterface = nameof(FancyNewInterface);
-
     public const string RegisteredButNotInAppConfig = nameof(RegisteredButNotInAppConfig);
     public const string RegisteredAndNullInAppConfig = nameof(RegisteredAndNullInAppConfig);
     public const string RegisteredAndTrueInAppConfig = nameof(RegisteredAndTrueInAppConfig);
