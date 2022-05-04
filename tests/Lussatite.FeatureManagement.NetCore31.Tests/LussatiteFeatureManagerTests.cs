2cf969462cb69a02d26ffcf331bce74a4949626d
Thomas Harold <tgharold@gmail.com>
2022-05-04 12:55:54 -0400

    move Framework tests to separate folder, refactors

--- tests/Lussatite.FeatureManagement.NetCore31.Tests/LussatiteFeatureManagerTests.cs
+++ tests/Lussatite.FeatureManagement.NetCore31.Tests/LussatiteFeatureManagerTests.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Lussatite.FeatureManagement.SessionManagers;
-using Lussatite.FeatureManagement.TestingCommon;
 using Microsoft.Extensions.Configuration;
 using Xunit;
 
