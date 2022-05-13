4fb3e39b2aa76555f365450f18e804b20e393457
Thomas Harold <tgharold@gmail.com>
2022-05-13 06:49:02 -0400

    Add DataReaderSessionManager
    By offloading the SqlCommand creation to a factory method that gets
    passed in, it's up to the caller to provide the SQL statement which
    retrieves the row.
    
    Trying to parameterize SQL table names is really difficult without
    introducing SQL injection vulnerabilities, so I'm offloading that
    to the caller.

---
+++ src/Lussatite.FeatureManagement.SessionManagers.Dapper/DataReaderSessionManagerSettings.cs
@@ -0,0 +1,10 @@
+
+// ReSharper disable once CheckNamespace
+namespace Lussatite.FeatureManagement.SessionManagers.DataReader
+{
+    /// <summary>Settings class for the <see cref="DataReaderSessionManager"/> instance.</summary>
+    public class DataReaderSessionManagerSettings
+    {
+        public string FeatureValueColumn { get; set; } = "Enabled";
+    }
+}
