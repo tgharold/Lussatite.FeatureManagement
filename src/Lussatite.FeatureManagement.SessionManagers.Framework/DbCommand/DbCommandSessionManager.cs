9e8011d72cce63ede9e0d20e1c1599d1ff6d1dd2
Thomas Harold <tgharold@gmail.com>
2022-05-13 00:29:03 -0400

    Update notes on the DbCommand classes

--- src/Lussatite.FeatureManagement.SessionManagers.Framework/DbCommand/DbCommandSessionManager.cs
+++ src/Lussatite.FeatureManagement.SessionManagers.Framework/DbCommand/DbCommandSessionManager.cs
@@ -10,8 +10,21 @@ using System.Threading.Tasks;
 // ReSharper disable once CheckNamespace
 namespace Lussatite.FeatureManagement.SessionManagers.Framework
 {
+    /// <summary><para>WARN: This class is still under active development and will likely change
+    /// names before the final release.</para>
+    /// <para>A read-only implementation of <see cref="ISessionManager"/> which looks
+    /// at a database table to obtain the values.  The database table schema/name and column
+    /// names can be configured via <see cref="DbCommandSessionManagerSettings"/>.</para>
+    /// </summary>
     public class DbCommandSessionManager : ISessionManager
     {
+        private readonly DbCommandSessionManagerSettings _settings;
+
+        public DbCommandSessionManager(DbCommandSessionManagerSettings settings = null)
+        {
+            _settings = settings ?? new DbCommandSessionManagerSettings();
+        }
+
         public async Task<bool?> GetAsync(string featureName)
         {
             // "select FeatureName, Feature from [{schema}].[{tableName}]"
--- src/Lussatite.FeatureManagement.SessionManagers.Framework/DbCommand/DbCommandSessionManagerSettings.cs
+++ src/Lussatite.FeatureManagement.SessionManagers.Framework/DbCommand/DbCommandSessionManagerSettings.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 // ReSharper disable once CheckNamespace
 namespace Lussatite.FeatureManagement.SessionManagers.Framework
 {
+    /// <summary>Settings class for the <see cref="DbCommandSessionManager"/> instance.</summary>
     public class DbCommandSessionManagerSettings
     {
-        public string SchemaName { get; set; } = "dbo";
-        public string TableName { get; set; } = "FeatureManagement";
+        public string TableName { get; set; } = "dbo.FeatureManagement";
+
         public string FeatureNameColumn { get; set; } = "FeatureName";
+
         public string FeatureValueColumn { get; set; } = "Enabled";
     }
 }
