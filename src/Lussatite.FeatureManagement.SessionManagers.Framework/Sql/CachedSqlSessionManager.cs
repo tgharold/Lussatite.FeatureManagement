76bf2a7ac6dc8832daa6d320aa20ec76a7dbdb4f
Thomas Harold <tgharold@gmail.com>
2022-05-19 14:58:10 -0400

    Add a CachedSqlSessionManager ISessionManager

---
+++ src/Lussatite.FeatureManagement.SessionManagers.Framework/Sql/CachedSqlSessionManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Data.Common;
+using System.Threading.Tasks;
+using LazyCache;
+
+// ReSharper disable once CheckNamespace
+namespace Lussatite.FeatureManagement.SessionManagers.Framework
+{
+    /// <summary>A caching version of <see cref="SqlSessionManager"/> which reduces the
+    /// number of calls to the underlying database.</summary>
+    public class CachedSqlSessionManager : SqlSessionManager
+    {
+        //NOTE: This is a copy of the one from Lussatite.FeatureManagement.SessionManagers.Core
+        //Because DbCommand is not .NET Standard 2.0, we are splitting along Framework/Core lines.
+
+        private readonly IAppCache _cache;
+        private readonly CachedSqlSessionManagerSettings _cachedSettings;
+
+        public CachedSqlSessionManager(
+            Func<string, DbCommand> getValueCommandFactory,
+            CachedSqlSessionManagerSettings settings = null,
+            IAppCache cache = null
+            ) : base(getValueCommandFactory, settings)
+        {
+            _cachedSettings = settings ?? new CachedSqlSessionManagerSettings();
+            if (_cachedSettings.CacheTime.Seconds <= 0)
+                throw new ArgumentOutOfRangeException(nameof(_cachedSettings.CacheTime));
+            _cache = cache ?? new CachingService();
+        }
+
+        public override async Task<bool?> GetAsync(string featureName)
+        {
+            if (string.IsNullOrWhiteSpace(featureName)) return false;
+            var cacheKey = $"Lussatite.FeatureManagement:{nameof(CachedSqlSessionManager)}:{featureName}";
+            var absoluteExpiration = DateTimeOffset.UtcNow.Add(_cachedSettings.CacheTime);
+
+            return await _cache.GetOrAddAsync(
+                expires: absoluteExpiration,
+                key: cacheKey,
+                addItemFactory: async () => await base.GetAsync(featureName).ConfigureAwait(false)
+                );
+        }
+    }
+}
