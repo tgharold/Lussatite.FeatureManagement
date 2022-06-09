a6a18d5e9bddfb1941fbf38c1ba9ecf60fc9bc97
Thomas Harold <tgharold@gmail.com>
2022-06-09 07:16:47 -0400

    Create randomly named test database
    The database name will be "test-yyyyMMdd-hhmm-{random}" based on
    the current UTC instant.  This keeps from cluttering up any shared
    SQL database too badly as the databases will have the date in the name.
    
    If the test run is successful, the database gets dropped during Dispose().

--- tests/TestCommon.Standard/MicrosoftSQLServer/SQLServerDatabaseFixture.cs
+++ tests/TestCommon.Standard/MicrosoftSQLServer/SQLServerDatabaseFixture.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Data.SQLite;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Lussatite.FeatureManagement.SessionManagers;
 using Lussatite.FeatureManagement.SessionManagers.SQLite;
@@ -11,21 +12,164 @@ namespace TestCommon.Standard.MicrosoftSQLServer
     public class SqlServerDatabaseFixture : IDisposable
     {
         public SqlSessionManagerSettings SqlSessionManagerSettings { get; }
+        public string DbName { get; }
+
+        /// <summary>Used to create the database to be used for the tests.  The initial catalog
+        /// should be 'master' and the user account must have permissions to CREATE DATABASE.</summary>
+        private readonly string _masterConnectionString;
 
         public SqlServerDatabaseFixture()
         {
+            var now = DateTimeOffset.UtcNow;
+            DbName = $"test-{now:yyyyMMdd}-{now:HHmm}-{UpperCaseAlphanumeric(8)}";
+
+            //TODO: Pull from configuration connection strings "TestSqlServerDatabaseMaster"
+            _masterConnectionString = "server=localhost,11433;database=master;user=sa;password=Pass123!";
+
+            var connectionString = CreateConnectionStringFromOriginal(_masterConnectionString, DbName);
+
             SqlSessionManagerSettings = new SQLiteSessionManagerSettings
             {
-                // Using a name and a shared cache allows multiple connections to access the same in-memory database
-                ConnectionString = $"Data Source=test-{Guid.NewGuid()};Mode=Memory;Cache=Shared",
+                ConnectionString = connectionString,
 
                 EnableSetValueCommand = true,
             };
 
-            SqlSessionManagerSettings.CreateDatabaseTable(GetConnectionString());
+            CreateDatabase();
+            SqlSessionManagerSettings.CreateDatabaseTable(connectionString);
         }
 
-        public string GetConnectionString() => SqlSessionManagerSettings.ConnectionString;
+        private static readonly Random Random = new Random();
+
+        private static string UpperCaseAlphanumeric(int size)
+        {
+            string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            var chars = Enumerable.Range(0, size)
+                .Select(x => input[Random.Next(0, input.Length)]);
+            return new string(chars.ToArray());
+        }
+
+
+        private bool _databaseInitialized;
+        private readonly object _lock = new object();
+
+        private void CreateDatabase()
+        {
+            // https://en.wikipedia.org/wiki/Double-checked_locking
+            if (_databaseInitialized) return;
+            lock (_lock)
+            {
+                if (_databaseInitialized) return;
+
+                try
+                {
+                    if (!DatabaseExists())
+                    {
+                        using (var connection = new SqlConnection(_masterConnectionString))
+                        {
+                            connection.Open();
+                            using(var command = connection.CreateCommand())
+                            {
+                                command.CommandText = FormattableString.Invariant(
+                                    $"CREATE DATABASE [{DbName}];");
+                                command.ExecuteNonQuery();
+                            }
+
+                            var attempt = 1;
+                            while (!DatabaseIsAcceptingQueries())
+                            {
+                                attempt++;
+                                var sleepMilliseconds = Math.Min((int) (Math.Pow(1.2, attempt) * 100), 500);
+                                Thread.Sleep(sleepMilliseconds);
+                                if (attempt > 100)
+                                    throw new Exception(
+                                        $"Database '{DbName}' refused to execute queries!"
+                                    );
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.Data["dbName"] = DbName;
+                    throw;
+                }
+
+                _databaseInitialized = true;
+            }
+        }
+
+        private bool DatabaseExists()
+        {
+            using (var connection = new SqlConnection(_masterConnectionString))
+            {
+                // https://docs.microsoft.com/en-us/sql/t-sql/functions/db-id-transact-sql
+                // https://stackoverflow.com/a/2028955
+                using (var command = new SqlCommand($@"select DB_ID('{DbName}');", connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        var result = (int)command.ExecuteScalar();
+                        connection.Close();
+                        return (result > 0);
+                    }
+                    catch (SqlException)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private bool DatabaseIsAcceptingQueries()
+        {
+            using (var connection = new SqlConnection(SqlSessionManagerSettings.ConnectionString))
+            {
+                var command = new SqlCommand("select 1;", connection);
+                try
+                {
+                    connection.Open();
+                    var result = (int)command.ExecuteScalar();
+                    return (result == 1);
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+        private void DropDatabase()
+        {
+            using (var connection = new SqlConnection(_masterConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = FormattableString.Invariant(
+                        $@"ALTER DATABASE [{DbName}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE [{DbName}];"
+                        );
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private static string CreateConnectionStringFromOriginal(string connectionString, string dbName)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentNullException(nameof(connectionString));
+
+            if (string.IsNullOrWhiteSpace(dbName))
+                throw new ArgumentNullException(nameof(dbName));
+
+            var builder = new SqlConnectionStringBuilder(connectionString)
+            {
+                InitialCatalog = dbName
+            };
+
+            return builder.ConnectionString;
+        }
 
         /// <summary>Meant to be used as a debug step, this returns all of the data in the table.</summary>
         public async Task<List<object[]>> GetAllData()
@@ -67,6 +211,15 @@ namespace TestCommon.Standard.MicrosoftSQLServer
                 if (disposing)
                 {
                     // dispose managed state (managed objects)
+
+                    // https://en.wikipedia.org/wiki/Double-checked_locking#Usage_in_C#
+                    if (!_databaseInitialized) return;
+                    lock (_lock)
+                    {
+                        if (!_databaseInitialized) return;
+                        if (DatabaseExists()) DropDatabase();
+                        _databaseInitialized = false;
+                    }
                 }
 
                 // free unmanaged resources (unmanaged objects) and override finalizer
