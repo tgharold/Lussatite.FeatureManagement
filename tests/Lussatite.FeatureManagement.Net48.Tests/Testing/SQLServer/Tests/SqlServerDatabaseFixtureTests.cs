65a7a5fc9a8fc5ce17e25be0db1fc06bafaa84ec
Thomas Harold <tgharold@gmail.com>
2022-06-09 10:50:31 -0400

    Add fixture tests for .NET 4.8 / .NET Core 3.1

---
+++ tests/Lussatite.FeatureManagement.Net48.Tests/Testing/SQLServer/Tests/SqlServerDatabaseFixtureTests.cs
@@ -0,0 +1,34 @@
+using System.Data.SqlClient;
+using TestCommon.Standard.MicrosoftSQLServer;
+using Xunit;
+
+namespace Lussatite.FeatureManagement.Net48.Tests.Testing.SQLServer.Tests
+{
+    [Collection(nameof(SQLServerDatabaseCollection))]
+    public class SqlServerDatabaseFixtureTests
+    {
+        private readonly SqlServerDatabaseFixture _dbFixture;
+
+        public SqlServerDatabaseFixtureTests(SqlServerDatabaseFixture dbFixture)
+        {
+            _dbFixture = dbFixture;
+        }
+
+        [Fact]
+        public void Can_execute_select_star_query_against_TableName()
+        {
+            var connectionString = _dbFixture.SqlSessionManagerSettings.ConnectionString;
+            var settings = _dbFixture.SqlSessionManagerSettings;
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var queryCommand = conn.CreateCommand();
+                queryCommand.CommandText =
+                    $@"SELECT * FROM [{settings.FeatureSchemaName}].[{settings.FeatureTableName}];";
+                var value = (string)queryCommand.ExecuteScalar();
+                conn.Close();
+            }
+        }
+    }
+}
