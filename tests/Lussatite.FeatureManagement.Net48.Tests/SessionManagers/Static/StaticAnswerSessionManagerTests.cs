4ea193f1d36996805f127bb2e6c701bd385151c6
Thomas Harold <tgharold@gmail.com>
2022-06-13 13:54:54 -0400

    StaticAnswerSessionManagerTests for netcore31/net48

---
+++ tests/Lussatite.FeatureManagement.Net48.Tests/SessionManagers/Static/StaticAnswerSessionManagerTests.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lussatite.FeatureManagement.SessionManagers;
+using Xunit;
+
+namespace Lussatite.FeatureManagement.Net48.Tests.SessionManagers.Static
+{
+    public class StaticAnswerSessionManagerTests
+    {
+        private const string TrueFeature = nameof(TrueFeature);
+        private const string FalseFeature = nameof(FalseFeature);
+        private const string NullFeature = nameof(NullFeature);
+
+        private readonly StaticAnswerSessionManager _sut = new StaticAnswerSessionManager(
+            new Dictionary<string, bool?>(StringComparer.OrdinalIgnoreCase)
+            {
+                [TrueFeature] = true,
+                [FalseFeature] = false,
+            });
+
+        [Theory]
+        [InlineData(null, NullFeature)]
+        [InlineData(true, TrueFeature)]
+        [InlineData(false, FalseFeature)]
+        public async Task GetAsync_returns_expected(bool? expected, string featureName)
+        {
+            var result = await _sut.GetAsync(featureName);
+            Assert.Equal(expected, result);
+        }
+    }
+}
+
